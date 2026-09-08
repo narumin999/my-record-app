6d61c2760a3644833a649ca4d1921ee1deff9544
narumin999 <narumin999@gmail.com>
2026-09-08 16:49:17 +0900

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    CLIENT_ID: '782455304746-gtiptf9v3vg3c4s3l6l9q66k8m8n4gt5.apps.googleusercontent.com',
+    MAP_API_KEY: 'AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', // ←ご自身のGoogle Maps APIキー
+    VIEWER_API_URL: '【ここに前回作成したGASのURLを貼り付けてください】',
+    SCOPES: 'https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/calendar.events'
+};
