ea973c621f08f5d2281a07d6a3f025de394d678f
narumin999 <narumin999@gmail.com>
2026-09-10 09:51:42 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@ const CONFIG = {
     CLIENT_ID: '782455304746-gtiptf9v3vg3c4s3l6l9q66k8m8n4gt5.apps.googleusercontent.com',
     MAP_API_KEY: 'AIzaSyBlghG_cpGSrAVBJl4l81V-6DLbg_BsOy0', // ←ご自身のGoogle Maps APIキー
     VIEWER_API_URL: 'https://script.google.com/macros/s/AKfycbzlTKnLv5NNlRQFC4DvmoimhflCJMLltOuDBxNwSbRvW3apZ8KnlF2fg2k25FXsKPXP/exec',
-    SCOPES: 'https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/calendar.events'
-};
+    // 末尾に userinfo.email を追加して、前回のアカウントを記憶できるようにします
+    SCOPES: 'https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/userinfo.email'};
